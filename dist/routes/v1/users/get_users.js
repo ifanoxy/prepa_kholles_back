49ac67d34e9fdad4f1b88bb97e4d376815681daa
alwin (mac) <alwindoudou@gmail.com>
2024-12-29 22:36:50 +0100

    Compilation

--- dist/routes/v1/users/get_users.js
+++ dist/routes/v1/users/get_users.js
@@ -3,6 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = default_1;
 function default_1(app) {
     app.app.get("/v1/users", async (req, res) => {
+        const token = req.headers?.authorization?.split(' ')[1];
+        if (!token || !await app.isAuth(token)) {
+            res.status(401).send("Unauthorized");
+            return;
+        }
         const users = await app.server.database.users.getAll(undefined, ["first_name", "last_name", "group"]);
         res.status(200).json({ data: users });
     });
